868c4fa9550da1623e067954f2d199f6aa19e17b
lixiaoniu <lixiaoniu@edaijia-inc.cn>
2017-07-10 10:23:08 +0800

    Demo

---
+++ src/Demo.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zxy on 2017/7/10.
+ */
+public class Demo {
+    static public void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+/*
+* 如果类能够被解释器直接解释执行，那么这个类必须有主方法
+* class环境变量的作用：告诉Java解释器去哪个目录寻找字节码文件
+* 对main方法而言，只有static 和 public两个修饰符可以互换，其他都是固定的
+* */
