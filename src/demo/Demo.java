5b4f431ad1aaaa06b765c10f7c7bfc323a919d72
lixiaoniu <lixiaoniu@edaijia-inc.cn>
2017-08-02 14:17:48 +0800

    提交代码

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -11,6 +11,7 @@ public class Demo {
     static public void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Crazy Java");
+        System.out.println("提交代码");
     }
 }
 /*
