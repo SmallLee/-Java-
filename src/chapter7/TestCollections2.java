8420ca98a29dc7bddb69ea4ceb034f95144cbe2f
lixiaoniu <lixiaoniu@edaijia-inc.cn>
2017-08-09 16:41:15 +0800

    集合

--- src/chapter7/TestCollections2.java
+++ src/chapter7/TestCollections2.java
@@ -14,5 +14,12 @@ public class TestCollections2 {
         System.out.println(Collections.binarySearch(arrayList,10));//2
         System.out.println(Collections.max(arrayList));///13
         System.out.println(Collections.min(arrayList));//-1
+//        Collections.fill(arrayList,3);
+//        System.out.println(arrayList);//[3, 3, 3, 3]
+//        集合中指定元素出现的次数
+        System.out.println(Collections.frequency(arrayList,4));//1
+//        替换所有元素
+        Collections.replaceAll(arrayList, 4, 2);
+        System.out.println(arrayList);//[-1, 2, 10, 13]
     }
 }
